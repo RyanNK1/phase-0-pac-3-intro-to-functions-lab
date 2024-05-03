46596fc1b4994c3b2ba823d545507ba3a5fbceda
ryan <ryan.kimani0@gmail.com>
2024-05-03 13:00:50 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
-function shout(string) {
+// function shout(string) {
+//     return string.toUpperCase();
+// }
+// function whisper(string) {
+//     return string.toLowerCase();
+// }
+// function logShout(string){
+//     console.log(string.toUpperCase())
+// }
+// function logWhisper(string){
+//     console.log(string.toLowerCase())
+// }
+// function sayHiToHeadphonedRoommate(string){
+//     if (string.toLowerCase()===string){
+//         return "I can't hear you!"
+//     }
+//     if (string.toUpperCase()===string){
+//         return "YES INDEED!"
+//     }
+//     if (string==="Let's have dinner together!"){
+//         return "I would love to!"
+//     }
+// }
+function shout(string){
     return string.toUpperCase();
-}
-function whisper(string) {
+};
+function whisper(string){
     return string.toLowerCase();
-}
+};
 function logShout(string){
-    console.log(string.toUpperCase())
-}
+    console.log(string.toUpperCase());
+};
 function logWhisper(string){
     console.log(string.toLowerCase())
-}
+};
 function sayHiToHeadphonedRoommate(string){
-    if (string.toLowerCase()===string){
-        return "I can't hear you!"
-    }
-    if (string.toUpperCase()===string){
+    if (string === string.toUpperCase()) {
         return "YES INDEED!"
-    }
-    if (string==="Let's have dinner together!"){
+    }else if (string === string.toLowerCase()){
+        return "I can't hear you!"
+    }else {
         return "I would love to!"
     }
 }
-
-
+// completed review
